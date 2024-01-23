50fbe1ed65da291d7e6416255d9d47c8cd685304
Mohammad Ahmed Asif <35397800+mohdahmedasif@users.noreply.github.com>
2024-01-23 21:43:26 +0100

    23/01/2024 09:43 PM

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -10,6 +10,7 @@ import sqlalchemy
 import urllib.request
 import pandas as pd
 
+# Extracting CSV file from given file URL
 url = "https://www.mowesta.com/data/measure/mowesta-dataset-20221107.zip"
 zip_path = 'mowesta_dataset.zip'
 data_path = 'mowesta_data/data.csv'
@@ -20,17 +21,27 @@ with zipfile.ZipFile(zip_path, 'r') as zip_ref:
     zip_ref.extractall('mowesta_data')
 os.remove(zip_path)
 
+# Create DataFrame from the imported data
 fields = ["Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"]
 df = pd.read_csv(data_path, sep=";", index_col=False, usecols=fields, encoding='utf-8', decimal=",")
+
+# Reshape Data
+#Only use the columns "Geraet", "Hersteller", "Model", "Monat", "Temperatur in °C (DWD)", "Batterietemperatur in °C", "Geraet aktiv"
+#Rename "Temperatur in °C (DWD)" to "Temperatur"
+#Rename "Batterietemperatur in °C" to "Batterietemperatur"
+#There can be multiple temperature measurements per row
+#discard all columns to the right of “​​Geraet aktiv”
+
 df.rename(columns={"Temperatur in °C (DWD)": "Temperatur", "Batterietemperatur in °C": "Batterietemperatur"}, inplace=True)
 
+# Transform Data - Correct Temperature Conversion
 def celsius_to_fahrenheit(temp_cels):
     return (temp_cels * 9 / 5) + 32
 
 df['Temperatur'] = celsius_to_fahrenheit(df['Temperatur'])
 df['Batterietemperatur'] = celsius_to_fahrenheit(df['Batterietemperatur'])
 
-# Validate data
+# Validate Data
 df = df.loc[df['Geraet'] > 0]
 df = df.loc[(df['Monat'] >= 1) & (df['Monat'] <= 12)]
 df = df.loc[(df['Temperatur'] >= -459.67) & (df['Temperatur'] <= 212)]
@@ -38,6 +49,7 @@ df = df.loc[(df['Batterietemperatur'] >= -459.67) & (df['Batterietemperatur'] <=
 valid_gear_aktiv_values = ["Ja", "Nein"]
 df = df.loc[(df['Geraet aktiv'].isin(valid_gear_aktiv_values))]
 
+# Migrating the data into sqlite database
 df.to_sql('temperatures', 'sqlite:///temperatures.sqlite', if_exists='replace', index=False, dtype={
     "Geraet": sqlalchemy.BIGINT,
     "Hersteller": sqlalchemy.TEXT,
